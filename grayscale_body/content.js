4b559f0f1203c9fee3a00e4dbd3ba33ef1cf81eb
jazyac <jazy_ac@hotmail.com>
2018-05-02 21:44:53 +1200

    Grayscale chrome extension

---
+++ grayscale_body/content.js
@@ -0,0 +1,13 @@
+
+
+// $('*[id*=logo]:visible').each(function() {
+//     console.log($(this).attr('id')); 
+
+//  //   $(this).html('<img src="http://occasionalplanet.org/wp-content/uploads/2014/08/invisible-man.jpg" alt="Extension Image" />');
+// $(this).css("-webkit-filter","grayscale(50%)");
+// });
+
+
+
+
+$('body').css("-webkit-filter","grayscale(50%)");
---
+++ grayscale_body/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var turnON = document.getElementById('turnON');
+  turnON.addEventListener('click', function() {
+
+    
+ 
+  }, false);
+}, false);
