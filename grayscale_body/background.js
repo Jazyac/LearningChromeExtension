6c142e2d0ddec547ccd5f6b2f473944e7fa01de5
jazyac <jazy_ac@hotmail.com>
2018-05-08 21:47:28 +1200

    refactoring before introduction of chrome storage to hold state after session.

--- grayscale_body/background.js
+++ grayscale_body/background.js
@@ -1,19 +1,68 @@
 var isOn=false;
 
+initialize();
 
+
+
+
+function initialize(){
+
+    if(!isSet()){
+
+
+        // chrome.storage.local.set({isOn: false});
+
+    }
+
+    
+}
+
+function isSet(){
+
+
+//   if( chrome.storage.local.get(['isOn']) == null){
+
+// return false;
+
+//   }
+
+// return true;
+}
+
+function returnIsOn(){
+
+    // return chrome.storage.local.get(['isOn']);
+     return isOn;
+ }
+ 
+ function setOnOff(){
+  
+//  if(isSet()){
+ 
+//     //  chrome.storage.local.set({isOn: !isOn});
+ 
+//  return;
+//  }
+ 
+//  initialize();
+ 
+isOn=!isOn;
+ 
+ 
+ }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
        
       if (request.message === "buttonClicked"){
-    
-         isOn=!isOn;
-         
+        
+         //isOn=!isOn;
+        setOnOff();
     
         
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             var activeTab = tabs[0];
-            chrome.tabs.sendMessage(activeTab.id, {"stateChanged": isOn});
+            chrome.tabs.sendMessage(activeTab.id, {"stateChanged": returnIsOn()});
           });
             
       
